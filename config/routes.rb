ce2443e738251d9a8d8471f3306a77ddef292caa
Victor Arriagada Martinez <veamchile@gmail.com>
2021-10-19 21:09:01 -0300

    Se configuro devise, dashboard, y bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get "dashboard/index"
+  devise_for :users
+  resources :admin, as: "projects"
+  authenticated :user do
+    root "dashboard#index", as: :authenticated_root
+  end
+
+  get "home/index"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "home#index"
 end
