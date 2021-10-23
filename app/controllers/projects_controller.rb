893a518185ec0284283ef0003515f6c6fc178afb
Victor Arriagada Martinez <veamchile@gmail.com>
2021-10-22 22:18:34 -0300

    Terminado con los objetivos planteados

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,14 +2,46 @@ class ProjectsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_project, only: %i[ show edit update destroy ]
 
+  def obtener_status
+  end
+
   # GET /projects or /projects.json
   def index
+    @estado_projecto = Hash.new()
     @projects = current_user.projects
+    @projectos = current_user.projects
+
+    @projectos.each do |projecto|
+      @test = projecto.tasks
+      @estados = Hash.new()
+      @test.each do |task|
+        @estados.merge!({ task.id => task.status })
+      end
+      if not(@estados.value?("in-progress") || @estados.value?("no-started"))
+        @estado_projecto.merge!({ projecto.id => "completed" })
+      elsif not(@estados.value?("in-progress") || @estados.value?("completed"))
+        @estado_projecto.merge!({ projecto.id => "no-started" })
+      else
+        @estado_projecto.merge!({ projecto.id => "in-progress" })
+      end
+    end
   end
 
   # GET /projects/1 or /projects/1.json
   def show
     @task = @project.tasks.build
+    @test = @project.tasks
+    @estados = Hash.new()
+    @test.each do |task|
+      @estados.merge!({ task.id => task.status })
+    end
+    if not(@estados.value?("in-progress") || @estados.value?("no-started"))
+      @estado_projecto = "completed"
+    elsif not(@estados.value?("in-progress") || @estados.value?("completed"))
+      @estado_projecto = "no-started"
+    else
+      @estado_projecto = "in-progress"
+    end
   end
 
   # GET /projects/new
